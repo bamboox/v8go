df3719ae5fd3fb8de2cacfda7a4d518de66f2a02
bamboox <bangbowan@gmail.com>
2025-04-10 14:11:41 +0800

    Update v8_upgrade.sh

--- deps/v8_upgrade.sh
+++ deps/v8_upgrade.sh
@@ -4,9 +4,10 @@ set -e
 
 current_v8_version="$(head -n1 VERSION | cut -d'-' -f1)"
 
-latest_chromium_version="$(wget -qO- "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Linux&num=1" | jq -r '.[0].version')"
-latest_v8_version="$(wget -qO- "https://chromiumdash.appspot.com/fetch_version?version=$latest_chromium_version" | jq -r '.v8_version')"
+#latest_chromium_version="$(wget -qO- "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Linux&num=1" | jq -r '.[0].version')"
+#latest_v8_version="$(wget -qO- "https://chromiumdash.appspot.com/fetch_version?version=$latest_chromium_version" | jq -r '.v8_version')"
 
+latest_v8_version="12.8.374.11"
 echo "Current version: $current_v8_version"
 echo "Latest version: $latest_v8_version"
 
